7e8885491ce97c0a025d589e1fc3b42a85fb8f1b
olustrrax <olutrrax@gmail.com>
2018-04-30 22:43:31 +0700

    add register page

---
+++ src/components/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="register">
+      <div class="container">
+        <div class="ui raised padded text container segment">
+            <h2 class="ui orange ribbon label">
+                ลงทะเบียน
+            </h2>
+        <p></p>
+        <p></p>
+        </div>
+      </div>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "register"
+}
+</script>
+
+<style>
+ /* .container{
+    width: 70%;
+    height: 70%;
+ } */
+</style>
