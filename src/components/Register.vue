2945496852acd763431cab2dfb49cca3f0603502
Phet 's Mac <emaiforphet@gmail.com>
2018-05-01 17:32:01 +0700

    Add firebase

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -7,6 +7,17 @@
             </h2>
         <p></p>
         <p></p>
+        <h2>SignUp</h2>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-md-offset-3"/>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-md-offset-3">
+
+            <input type="email" v-model="formData.email" class="form-control" placeholder="email">
+            <br>
+            <input type="password" v-model="formData.password" class="form-control" placeholder="password">
+            <br>
+            <button class="btn btn-success" @click="signUp">SignUp</button>
+        </div>
+    
         </div>
       </div>
     
@@ -16,7 +27,27 @@
 
 <script>
 export default {
-  name: "register"
+  name: "register",
+  data(){
+      return{
+          formData:{
+              email: '',
+              password: '',
+          }
+      }
+  },
+  methods:{
+      signUp(){
+          var thisfunc = this
+        firebase.auth().createUserWithEmailAndPassword(this.formData.email,this.formData.password)
+            .then( user=> {
+                thisfunc.$router.replace('/main')
+            })
+            .catch( e =>{
+                alert(e.message)
+            })
+      }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Main from '@/components/Main'
+import SignIn from '@/components/SignIn'
 import Register from '@/components/Register'
+
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
-    
     {
-      path: '/register',
-      name: 'Register',
-      component: Register
+      path: '/main',
+      name: 'main',
+      component: Main,
+      meta:{
+        requiresAuth : true
+      }
+    },
+    {
+      path:'/register',
+      name:'register',
+      component:Register
+    },
+    {
+      path:'*',
+      redirect:'/signin'
+    },
+    {
+      path:'/',
+      redirect:'/signin'
+    },
+    {
+      path:'/signin',
+      name:'SignIn',
+      component:SignIn
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth && !currentUser) {
+    next('signin')
+  } else if (!requiresAuth && currentUser) {
+    next('main')
+  } else {
+    next()
+  }
+})
+
+export default router
